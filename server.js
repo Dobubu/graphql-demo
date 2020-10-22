74c132b9cee470931f1a9f25bd7d10fd3292208c
mischa.huang <mischa.huang@creativearktech.com>
2020-10-22 12:20:50 +0800

    feat: use schema, resolver

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const model = require('./model')
 
 const typeDefs = gql`
   type Query {
     hello: String
+    me: User
+    users: [User]
+  }
+
+  type User {
+    id: ID!
+    name: String
+    email: String
+    gender: String
+    friends: [User]
+    height: Float
+    weight: Float
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    me:() => model.getUsers()[0],
+    users:() => model.getUsers(),
   },
 };
 
@@ -18,6 +33,6 @@ const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4001 }, () =>
+app.listen({ port: 4000 }, () =>
   console.log('Now browse to http://localhost:4000' + server.graphqlPath)
 );
